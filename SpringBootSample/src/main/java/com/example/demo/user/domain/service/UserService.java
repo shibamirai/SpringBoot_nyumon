db68c11bc21f9274dcdac747f5f086551ad7e3fb
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-05-30 15:27:54 +0900

    10-2

---
+++ SpringBootSample/src/main/java/com/example/demo/user/domain/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.demo.user.domain.service;
+
+import com.example.demo.user.domain.model.MUser;
+
+public interface UserService {
+    /** ユーザー登録 */
+    public void signup(MUser user);
+}
---
+++ SpringBootSample/src/main/java/com/example/demo/user/repository/UserMapper.java
@@ -0,0 +1,11 @@
+package com.example.demo.user.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.user.domain.model.MUser;
+
+@Mapper
+public interface UserMapper {
+    /** ユーザー登録 */
+    public int insertOne(MUser user);
+}
