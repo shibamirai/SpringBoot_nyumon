e4faed8d0f335d9eddb7d90cab0e3c08bbe46c1b
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-05-30 13:20:25 +0900

    8-1

--- SpringBootSample/src/main/java/com/example/demo/user/controller/SignupController.java
+++ SpringBootSample/src/main/java/com/example/demo/user/controller/SignupController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,7 +42,8 @@ public class SignupController {
 
     /** ユーザー登録処理 */
     @PostMapping("/signup")
-    public String postSignup(Model model, @ModelAttribute SignupForm form, BindingResult bindingResult) {
+    public String postSignup(Model model, @ModelAttribute @Validated SignupForm form,
+            BindingResult bindingResult) {
         // 入力チェック結果
         if (bindingResult.hasErrors()) {
             // NG：ユーザー登録画面に戻ります
