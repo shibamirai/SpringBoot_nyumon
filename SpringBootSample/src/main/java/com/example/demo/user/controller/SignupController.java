5a306d06903ef0492dd4fc06559e5ca872b587de
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-06-02 16:36:33 +0900

    14-6

--- SpringBootSample/src/main/java/com/example/demo/user/controller/SignupController.java
+++ SpringBootSample/src/main/java/com/example/demo/user/controller/SignupController.java
@@ -1,16 +1,25 @@
 package com.example.demo.user.controller;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Map;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.user.application.UserApplicationService;
 import com.example.demo.user.domain.model.MUser;
@@ -60,5 +69,50 @@ public class SignupController {
 
         // ログイン画面にリダイレクト
         return "redirect:/login";
+    }
+
+    /** ユーザーID重複の例外処理 */
+    @ExceptionHandler(DuplicateKeyException.class)
+    public String duplicateKeyExceptionHandler(DuplicateKeyException e, Model model,
+            HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        // 入力内容の取得
+        SignupForm form = generateFormFromRequest(request);
+        redirectAttributes.addFlashAttribute("signupForm", form);
+        // エラーメッセージ
+        String errorMessage = "このユーザーIDは既に使用されています。";
+        redirectAttributes.addFlashAttribute("errorMessage", errorMessage);
+        return "redirect:/user/signup";
+    }
+
+    /** その他の例外処理 */
+    @ExceptionHandler(Exception.class)
+    public String allExceptionHandler(Exception e, Model model,
+            HttpServletRequest request, RedirectAttributes redirectAttributes) {
+
+        // 入力内容の取得
+        SignupForm form = generateFormFromRequest(request);
+        redirectAttributes.addFlashAttribute("signupForm", form);
+        // エラーメッセージ
+        String errorMessage = "システムエラーが発生しました。";
+        redirectAttributes.addFlashAttribute("errorMessage", errorMessage);
+        return "redirect:/user/signup";
+    }
+
+    /** リクエストからSignupFormを生成する */
+    private SignupForm generateFormFromRequest(HttpServletRequest request) {
+        // リクエストの値をFormにセットする
+        SignupForm form = new SignupForm();
+        form.setUserId(request.getParameter("userId"));
+        form.setPassword(request.getParameter("password"));
+        form.setUserName(request.getParameter("userName"));
+        form.setAge(Integer.valueOf(request.getParameter("age")));
+        form.setGender(Integer.valueOf(request.getParameter("gender")));
+        // 日付文字列からDateを生成して、Formにセットする
+        String birthdayString = request.getParameter("birthday");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDate localDate = LocalDate.parse(birthdayString, formatter);
+        Date birthday = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        form.setBirthday(birthday);
+        return form;
     }
 }
